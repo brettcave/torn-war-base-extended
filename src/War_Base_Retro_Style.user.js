be948cc3dcf2bd8f294c7b3fccf2e190de4f6f9d
Vinkuun <vinkuun@fantasymail.de>
2014-08-09 18:40:19 +0200

    added toggle feature

--- src/War_Base_Retro_Style.user.js
+++ src/War_Base_Retro_Style.user.js
@@ -4,14 +4,60 @@
 // @author      Vinkuun [1791283]
 // @description Brings back the old war base layout
 // @include     *.torn.com/factions.php?step=your
-// @version     1.0.0
+// @version     1.1.0
 // @grant       GM_addStyle
 // ==/UserScript==
 
-GM_addStyle(
-  ".f-war-list { margin-top: 10px }" +
-  ".f-war-list > li, .f-war-list > li.first-in-row { margin: 10px 0; padding: 0; height: auto; width: auto }" +
-  ".f-war-list > li .status-wrap { display: none }" +
-  ".f-war-list > li .desc-wrap { display: block !important }" +
-  ".first-in-row { margin: 0; padding: 0 }" 
-);
+(function($, localStorage, window) {
+    'use strict';
+    
+    // returns true if the layout is enabled, false if not
+    function isEnabled() {
+      return JSON.parse(localStorage['vinkuunWarBaseRetroLayout'] || 'true');   
+    }
+    
+    // add the css used to get the old style back
+    GM_addStyle(
+      '.oldWarBase .f-war-list { margin-top: 10px }' +
+      '.oldWarBase .f-war-list > li, .oldWarBase .f-war-list > li.first-in-row { margin: 10px 0; padding: 0; height: auto; width: auto }' +
+      '.oldWarBase .f-war-list > li .status-wrap { display: none }' +
+      '.oldWarBase .f-war-list > li .desc-wrap { display: block !important }' +
+      '.oldWarBase .first-in-row { margin: 0; padding: 0 }' 
+    );
+    
+    var $main = $('#faction-main');
+    
+    // enables the old layout after the page has loaded
+    if (isEnabled()) {
+      $main.addClass('oldWarBase');   
+    }
+    
+    // button used to toggle the old/new layout
+    var $toggleButton = $('<button></button>', {text: 'Toggle war base layout'}).on('click', function() {                
+        if (isEnabled()) {
+            localStorage['vinkuunWarBaseRetroLayout'] = false;
+            $main.removeClass('oldWarBase');
+        } else {
+            localStorage['vinkuunWarBaseRetroLayout'] = true;
+            $main.addClass('oldWarBase');
+        }
+    });
+    
+    // add the button after the tabs
+    $main.before($('<p></p>', {style: 'text-align: center; font-size: 12px;'}).append($toggleButton));
+    
+    // show/hide the button according to the button
+    var urlChangeCallback = function () {
+        if (window.location.hash === '#/tab=main' || window.location.hash === '') {
+            $toggleButton.show();
+        } else {
+            $toggleButton.hide();
+        }
+    };
+    
+    // call it 1 time to show/hide the button after the page has loaded
+    urlChangeCallback();
+    
+    // listen to a change
+    window.onhashchange = urlChangeCallback;
+})($, localStorage, window);
