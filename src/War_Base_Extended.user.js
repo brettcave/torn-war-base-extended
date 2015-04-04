6d0e85267bf95476575738b350d83626f83f7d74
Matthew Musgrove <mr.muskrat@gmail.com>
2015-04-04 15:35:49 -0500

    Add a federal prison filter
    This is a small change but it adds a useful feature: filtering those individuals in federal prison.

--- src/War_Base_Extended.user.js
+++ src/War_Base_Extended.user.js
@@ -118,6 +118,13 @@ function applyFilter() {
     items.parent().parent().hide();
   }
 
+  if (warBaseFilter.status.federal) {
+    items = $list.find('span:contains("Federal")');
+    countFiltered += items.length;
+    
+    items.parent().parent().hide();
+  }
+    
   if (warBaseFilter.status.hospital) {
     $list.find('span:contains("Hospital")').each(function() {
       var $this = $(this);
@@ -159,6 +166,14 @@ function addFilterPanel($panel) {
   var $okayElement = $('<label>', {text: 'okay'}).prepend($okayCheckbox);
   $panel.append($okayElement).append(' or ');
 
+  // status: federal filter
+  var $federalCheckbox = $('<input>', {type: 'checkbox'})
+    .on('change', function() {
+      reapplyFilter({status: {federal: this.checked}});
+    });
+  var $federalElement = $('<label>', {text: 'federal'}).prepend($federalCheckbox);
+  $panel.append($federalElement).append(', ');
+   
   // status: hospital filter
   var $hospitalTextfield = $('<input>', {type: 'number', style: 'width: 50px'})
     .on('change', function() {
@@ -178,6 +193,7 @@ function addFilterPanel($panel) {
   // set the states of the elements according to the saved filter
   $travelingCheckbox[0].checked = warBaseFilter.status.traveling || false;
   $okayCheckbox[0].checked = warBaseFilter.status.okay || false;
+  $federalCheckbox[0].checked = warBaseFilter.status.federal || false;
   $hospitalTextfield.val(warBaseFilter.status.hospital || '');
 }
 
